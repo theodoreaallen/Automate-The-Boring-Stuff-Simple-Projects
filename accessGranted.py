24df560b747795c29871ac1e0d0665a8d15f5e88
Theodore Allen <87066315+theodoreaallen@users.noreply.github.com>
2021-07-21 10:18:22 +0100

    Add files via upload

---
+++ accessGranted.py
@@ -0,0 +1,16 @@
+name = ''
+while name != 'Theo':
+    print('Insert Name')
+    name = input()
+    if name == 'Theo':
+        print('Hello, Theo!')
+    else:
+        print('Access Denied')
+password = ''
+while password != 'Swordfish':
+    print('Insert Password')
+    password = input()
+    if password == 'Swordfish':
+        print('Access Granted')
+    else:
+        print('Access Denied')
